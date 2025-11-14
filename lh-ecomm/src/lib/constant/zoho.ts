3d63dbaa4b572cc1a25803ff209cf327239999c5
Tify.id <tify.product@gmail.com>
2025-11-14 22:50:26 +0700

    Add files via upload

---
+++ lh-ecomm/src/lib/constant/zoho.ts
@@ -0,0 +1,331 @@
+export const SERVICES_DROPDOWN_LIST = [
+	{ value: 'direct-sell', name: 'Direct Sell' },
+	{ value: 'consignment', name: 'Consignment' },
+	{ value: 'trade-in', name: 'Trade In' }
+];
+
+export const SWU_PRODUCT_DROPDOWN_LIST = [
+	{ value: 'Watch', name: 'Watch' },
+	{ value: 'Hermes', name: 'Hermès' }
+];
+
+export const BUYER_PRODUCT_CATEGORY_DROPDOWN_LIST = [
+	{ value: 'Watch', name: 'Watch' },
+	{ value: 'Bag', name: 'Bag' },
+	{ value: 'Jewelry', name: 'Jewelry' },
+	{ value: 'Beauty', name: 'Beauty' }
+];
+
+export const WATCH_CONDITION_DROPDOWN_LIST = [
+	{ value: 'unworn', name: 'Unworn (Super Mint Condition without any sign of wear)' },
+	{ value: 'verygood', name: 'Very Good(Worn with little to no signs of wear)' },
+	{ value: 'good', name: 'Good (Light signs of wear of scratches)' },
+	{ value: 'fair', name: 'Fair (Obvious signs of wear or scratches)' },
+	{ value: 'poor', name: 'Poor (Heavy signs of wear or scratches)' }
+];
+
+export const COMPLETENESS_OPTIONS = [
+	'Box',
+	'Paper & Certificate',
+	'Warranty Card',
+	'Original Store Receipt'
+];
+
+export const MODEL_DROPDOWN_LIST = [
+	{ value: 'Birkin', name: 'Birkin' },
+	{ value: 'Constance', name: 'Constance' },
+	{ value: 'Constance Slim', name: 'Constance Slim' },
+	{ value: 'Constance To Go', name: 'Constance To Go' },
+	{ value: 'Evelyne', name: 'Evelyne' },
+	{ value: 'Garden Party', name: 'Garden Party' },
+	{ value: 'Herbag', name: 'Herbag' },
+	{ value: 'In The Loop', name: 'In The Loop' },
+	{ value: 'Jypsiere', name: 'Jypsiere' },
+	{ value: 'Kelly', name: 'Kelly' },
+	{ value: 'Kelly Cut', name: 'Kelly Cut' },
+	{ value: 'Kelly To Go', name: 'Kelly To Go' },
+	{ value: 'Lindy', name: 'Lindy' },
+	{ value: 'Mini Lindy', name: 'Mini Lindy' },
+	{ value: 'Picotin', name: 'Picotin' }
+];
+
+export const HARDWARE_DROPDOWN_LIST = [
+	{ value: 'BHW', name: 'BHW' },
+	{ value: 'GHW', name: 'GHW' },
+	{ value: 'PHW', name: 'PHW' },
+	{ value: 'RGHW', name: 'RGHW' },
+	{ value: 'PBHW', name: 'PBHW' },
+	{ value: 'Others', name: 'Others' }
+];
+
+export const DATESTAMPS_DROPDOWN_LIST = [
+	{ value: 'C_2018', name: 'C (2018)' },
+	{ value: 'D_2019', name: 'D (2019)' },
+	{ value: 'Y_2020', name: 'Y (2020)' },
+	{ value: 'Z_2021', name: 'Z (2021)' },
+	{ value: 'U_2022', name: 'U (2022)' },
+	{ value: 'B_2023', name: 'B (2023)' },
+	{ value: 'W_2024', name: 'W (2024)' }
+];
+
+export const SIZE_DROPDOWN_LIST = [
+	{ value: '14', name: '14' },
+	{ value: '18', name: '18' },
+	{ value: '20', name: '20' },
+	{ value: '22', name: '22' },
+	{ value: '24', name: '24' },
+	{ value: '25', name: '25' },
+	{ value: '26', name: '26' },
+	{ value: '28', name: '28' },
+	{ value: '30', name: '30' },
+	{ value: '32', name: '32' },
+	{ value: '36', name: '36' }
+];
+
+export const LEATHER_DROPDOWN_LIST = [
+	{ value: 'Alligator', name: 'Alligator' },
+	{ value: 'Box', name: 'Box' },
+	{ value: 'Clemence', name: 'Clemence' },
+	{ value: 'Epsom', name: 'Epsom' },
+	{ value: 'Evercalf', name: 'Himalaya' },
+	{ value: 'Lizzard', name: 'Lizzard' },
+	{ value: 'Ostrich', name: 'Ostrich' },
+	{ value: 'Swift', name: 'Swift' },
+	{ value: 'Togo', name: 'Togo' },
+	{ value: 'Madame', name: 'Madame' },
+	{ value: 'Maurice', name: 'Maurice' },
+	{ value: 'Evercolor', name: 'Evercolor' },
+	{ value: 'Chevre', name: 'Chevre' },
+	{ value: 'Other', name: 'Other' }
+];
+
+export const HERMES_COMPLETENESS_DROPDOWN_LIST = [
+	{ value: 'Box', name: 'Box' },
+	{ value: 'Original Dustbag', name: 'Original Dustbag' },
+	{ value: 'Tag', name: 'Tag' },
+	{ value: 'Flanel', name: 'Flanel' },
+	{ value: 'Original Store Receipt', name: 'Original Store Receipt' },
+	{ value: 'Carecard', name: 'Carecard' },
+	{ value: 'Raincoat', name: 'Raincoat' },
+	{ value: 'Authenticity Card', name: 'Authenticity Card' },
+	{ value: 'Strap', name: 'Strap' },
+	{ value: 'Booklet', name: 'Booklet' }
+];
+
+export const HERMES_CONDITION_DROPDOWN_LIST = [
+	{ value: 'Brand New', name: 'Brand New(never used may come with box)' },
+	{ value: 'Like New', name: 'Like New (used once or twice as good as new used with care)' },
+	{ value: 'Well used', name: 'Well used(has minor flaws or defects)' },
+	{ value: 'Heavily Used', name: 'Heavily used (has obvious signs of use or defects)' }
+];
+
+export const CONSIGNMENT_BRAND_DROPDOWN_LIST = [
+	{ value: 'A. LANGE & SÖHNE', name: 'A. LANGE & SÖHNE' },
+	{ value: 'ANGELUS', name: 'ANGELUS' },
+	{ value: 'ARTISANS DE GENÈVE', name: 'ARTISANS DE GENÈVE' },
+	{ value: 'ATELIER WEN', name: 'ATELIER WEN' },
+	{ value: 'Audemars Piguet', name: 'AUDEMARS PIGUET' },
+	{ value: 'BALTIC', name: 'BALTIC' },
+	{ value: 'BAMFORD', name: 'BAMFORD' },
+	{ value: 'BELL & ROSS', name: 'BELL & ROSS' },
+	{ value: 'BLANCPAIN', name: 'BLANCPAIN' },
+	{ value: 'BREGUET', name: 'BREGUET' },
+	{ value: 'BREITLING', name: 'BREITLING' },
+	{ value: 'BULGARI', name: 'BULGARI' },
+	{ value: 'Cartier', name: 'CARTIER' },
+	{ value: 'CHOPARD', name: 'CHOPARD' },
+	{ value: 'DE BETHUNE', name: 'DE BETHUNE' },
+	{ value: 'Franck Muller', name: 'FRANCK MULLER' },
+	{ value: 'F.P. JOURNE', name: 'F.P. JOURNE' },
+	{ value: 'GIRARD_PERREGAUX', name: 'GIRARD-PERREGAUX' },
+	{ value: 'GREUBEL FORSEY', name: 'GREUBEL FORSEY' },
+	{ value: 'GRAND_SEIKO', name: 'GRAND SEIKO' },
+	{ value: 'HERMES', name: 'HERMÈS' },
+	{ value: 'H MOSER & CIE', name: 'H MOSER & CIE' },
+	{ value: 'Hublot', name: 'HUBLOT' },
+	{ value: 'HYT', name: 'HYT' },
+	{ value: 'IWC', name: 'IWC' },
+	{ value: 'JACOB & CO.', name: 'JACOB & CO.' },
+	{ value: 'JAEGER-LECOULTRE', name: 'JAEGER-LECOULTRE' },
+	{ value: 'JAQUET DROZ', name: 'JAQUET DROZ' },
+	{ value: 'MB&F', name: 'MB&F' },
+	{ value: 'MONTBLANC', name: 'MONTBLANC' },
+	{ value: 'Omega', name: 'OMEGA' },
+	{ value: 'Panerai', name: 'PANERAI' },
+	{ value: 'PARMIGIANI FLEURIER', name: 'PARMIGIANI FLEURIER' },
+	{ value: 'Patek Philippe', name: 'PATEK PHILIPPE' },
+	{ value: 'PIAGET', name: 'PIAGET' },
+	{ value: 'RICHARD_MILLE', name: 'RICHARD MILLE' },
+	{ value: 'ROGER_DUBUIS', name: 'ROGER DUBUIS' },
+	{ value: 'Rolex', name: 'ROLEX' },
+	{ value: 'Tag Heuer', name: 'TAG HEUER' },
+	{ value: 'Tudor', name: 'TUDOR' },
+	{ value: 'ULYSSE NARDIN', name: 'ULYSSE NARDIN' },
+	{ value: 'UNIMATIC', name: 'UNIMATIC' },
+	{ value: 'URWERK', name: 'URWERK' },
+	{ value: 'VACHERON_CONSTANTIN', name: 'VACHERON CONSTANTIN' },
+	{ value: 'VAN_CLEEF_ARPELS', name: 'VAN CLEEF & ARPELS' },
+	{ value: 'Zenith', name: 'ZENITH' },
+	{ value: 'OTHERS', name: 'OTHERS' }
+];
+
+export const TRADE_IN_BRAND_DROPDOWN_LIST = [
+	{ value: 'A. LANGE & SÖHNE', name: 'A. LANGE & SÖHNE' },
+	{ value: 'Audermars Piguet', name: 'AUDEMARS PIGUET' },
+	{ value: 'BELL & ROSS', name: 'BELL & ROSS' },
+	{ value: 'BREITLING', name: 'BREITLING' },
+	{ value: 'Cartier', name: 'CARTIER' },
+	{ value: 'CHOPARD', name: 'CHOPARD' },
+	{ value: 'Franck Muller', name: 'FRANCK MULLER' },
+	{ value: 'F.P. JOURNE', name: 'F.P. JOURNE' },
+	{ value: 'GIRARD_PERREGAUX', name: 'GIRARD-PERREGAUX' },
+	{ value: 'Hublot', name: 'HUBLOT' },
+	{ value: 'IWC', name: 'IWC' },
+	{ value: 'JACOB & CO.', name: 'JACOB & CO.' },
+	{ value: 'JAEGER-LECOULTRE', name: 'JAEGER-LECOULTRE' },
+	{ value: 'MB&F', name: 'MB&F' },
+	{ value: 'Omega', name: 'OMEGA' },
+	{ value: 'Panerai', name: 'PANERAI' },
+	{ value: 'Patek Philippe', name: 'PATEK PHILIPPE' },
+	{ value: 'PIAGET', name: 'PIAGET' },
+	{ value: 'RICHARD_MILLE', name: 'RICHARD MILLE' },
+	{ value: 'ROGER_DUBUIS', name: 'ROGER DUBUIS' },
+	{ value: 'Rolex', name: 'ROLEX' },
+	{ value: 'Tag Heuer', name: 'TAG HEUER' },
+	{ value: 'Tudor', name: 'TUDOR' },
+	{ value: 'URWERK', name: 'URWERK' },
+	{ value: 'VACHERON_CONSTANTIN', name: 'VACHERON CONSTANTIN' },
+	{ value: 'Zenith', name: 'ZENITH' },
+	{ value: 'OTHERS', name: 'OTHERS' }
+];
+
+export const WATCH_BRAND_DROPDOWN_LIST = [
+	{ value: 'AET REMOULD', name: 'AET Remould' },
+	{ value: 'AUDEMARS PIGUET', name: 'Audemars Piguet' },
+	{ value: 'BELL & ROSS', name: 'Bell & Ross' },
+	{ value: 'BREITLING', name: 'Breitling' },
+	{ value: 'BULGARI', name: 'Bulgari' },
+	{ value: 'CARTIER', name: 'Cartier' },
+	{ value: 'CHANEL', name: 'Chanel' },
+	{ value: 'CHOPARD', name: 'Chopard' },
+	{ value: 'DIW', name: 'DiW' },
+	{ value: 'FRANCK MULLER', name: 'Franck Muller' },
+	{ value: 'GIRARD-PERREGAUX', name: 'Girard-Perregaux' },
+	{ value: 'GRAND SEIKO', name: 'Grand Seiko' },
+	{ value: 'HUBLOT', name: 'Hublot' },
+	{ value: 'IWC', name: 'IWC' },
+	{ value: 'JAEGER-LECOULTRE', name: 'Jaeger-LeCoultre' },
+	{ value: "L'EPPE", name: "L'Epee" },
+	{ value: 'LONGINES', name: 'Longines' },
+	{ value: 'OMEGA', name: 'Omega' },
+	{ value: 'PANERAI', name: 'Panerai' },
+	{ value: 'PATEK PHILIPPE', name: 'Patek Philippe' },
+	{ value: 'RICHARD MILLE', name: 'Richard Mille' },
+	{ value: 'ROGER DUBUIS', name: 'Roger Dubuis' },
+	{ value: 'ROLEX', name: 'Rolex' },
+	{ value: 'SKELETON CONCEPT', name: 'Skeleton Concept' },
+	{ value: 'TAG HEUER', name: 'Tag Heuer' },
+	{ value: 'TUDOR', name: 'TUDOR' },
+	{ value: 'VACHERON CONSTANTIN', name: 'Vacheron Constantin' },
+	{ value: 'VAN CLEEF & ARPELS', name: 'Van Cleef & Arpels' },
+	{ value: 'ZENITH', name: 'Zenith' },
+	{ value: 'OTHERS', name: 'OTHERS' }
+];
+
+export const BEAUTY_BRAND_DROPDOWN_LIST = [
+	{ value: 'AESOP', name: 'Aesop' },
+	{ value: 'APIVITA', name: 'Apivita' },
+	{ value: 'AVEDA', name: 'Aveda' },
+	{ value: 'BENEFIT', name: 'Benefit' },
+	{ value: 'BOBBI BROWN', name: 'Bobbi Brown' },
+	{ value: 'BURBERRY', name: 'Burberry' },
+	{ value: 'BVLGARI', name: 'Bvlgari' },
+	{ value: 'BY KILLIAN', name: 'By Killian' },
+	{ value: 'BYREDO', name: 'Byredo' },
+	{ value: 'CALVIN KLEIN', name: 'Calvin Klein' },
+	{ value: 'CAUDALIE', name: 'Caudalie' },
+	{ value: 'CHANTECAILLE', name: 'Chantecaille' },
+	{ value: 'CHARLOTTE TILBURY', name: 'Charlotte Tilbury' },
+	{ value: 'CHLOE', name: 'Chloe' },
+	{ value: 'CLARINS', name: 'Clarins' },
+	{ value: 'CLE DE PEAU', name: 'Cle de peau' },
+	{ value: 'CLE DE PEAU', name: 'Cle de Peau' },
+	{ value: 'CLINIQUE', name: 'Clinique' },
+	{ value: 'CLIO', name: 'Clio' },
+	{ value: 'COSME DECORTE', name: 'Cosme Decorte' },
+	{ value: 'DARPHIN', name: 'Darphin' },
+	{ value: 'DIOR', name: 'Dior' },
+	{ value: 'DIPTYQUE', name: 'Diptyque' },
+	{ value: 'DRUNK ELEPHANT', name: 'Drunk elephant' },
+	{ value: 'ELIZABETH ARDEN', name: 'Elizabeth Arden' },
+	{ value: 'ESTEE LAUDER', name: 'Estee lauder' },
+	{ value: 'EVE LOM', name: 'Eve Lom' },
+	{ value: 'FILORGA', name: 'Filorga' },
+	{ value: 'FIRST AID BEAUTY', name: 'First Aid Beauty' },
+	{ value: 'FRESH', name: 'Fresh' },
+	{ value: 'GIORGIO ARMANI', name: 'Giorgio Armani' },
+	{ value: 'GIVENCHY', name: 'Givenchy' },
+	{ value: 'GUCCI', name: 'Gucci' },
+	{ value: 'GUERLAIN', name: 'Guerlain' },
+	{ value: 'HACCI', name: 'Hacci' },
+	{ value: 'HELENA RUBINSTEIN', name: 'Helena Rubinstein' },
+	{ value: 'HERMES', name: 'Hermes' },
+	{ value: 'HOURGLASS', name: 'Hourglass' },
+	{ value: 'IPSA', name: 'Ipsa' },
+	{ value: 'JO MALONE', name: 'Jo Malone' },
+	{ value: 'KERASTASE', name: 'Kerastase' },
+	{ value: "KIEHL'S", name: "Kiehl's" },
+	{ value: "L'OCCITANE", name: "L'occitane" },
+	{ value: 'LA MER', name: 'La mer' },
+	{ value: 'LA PRAIRIE', name: 'La Prairie' },
+	{ value: 'LANCOME', name: 'Lancome' },
+	{ value: 'LANEIGE', name: 'Laneige' },
+	{ value: 'LANVIN', name: 'Lanvin' },
+	{ value: 'LAURA MERCIER', name: 'Laura Mercier' },
+	{ value: 'LE LABO', name: 'Le Labo' },
+	{ value: 'LOEWE', name: 'LOEWE' },
+	{ value: 'MAC', name: 'MAC' },
+	{ value: 'MAISON MARGIELA', name: 'Maison Margiela' },
+	{ value: 'MAKE UP FOR EVER', name: 'Make Up For Ever' },
+	{ value: 'MALIN + GOETZ', name: 'Malin + Goetz' },
+	{ value: 'MIU MIU', name: 'Miu Miu' },
+	{ value: 'NARS', name: 'Nars' },
+	{ value: 'NATURA BISSE', name: 'Natura Bisse' },
+	{ value: 'OBAGI', name: 'Obagi' },
+	{ value: 'OLAPLEX', name: 'OLAPLEX' },
+	{ value: 'ORIGINS', name: 'Origins' },
+	{ value: "PALMER'S", name: "Palmer's" },
+	{ value: "PENHALIGON'S", name: "PENHALIGON'S" },
+	{ value: 'PHILIP B', name: 'Philip B' },
+	{ value: 'PHILOSOPHY', name: 'Philosophy' },
+	{ value: 'PHYSIOGEL', name: 'Physiogel' },
+	{ value: 'POLA', name: 'POLA' },
+	{ value: 'RENE FURTERER', name: 'Rene Furterer' },
+	{ value: 'SHISEIDO', name: 'Shiseido' },
+	{ value: 'SHU UEMURA', name: 'Shu Uemura' },
+	{ value: 'SISLEY', name: 'Sisley' },
+	{ value: 'SK-II', name: 'SK-II' },
+	{ value: 'SKINCEUTICALS', name: 'SkinCeuticals' },
+	{ value: 'SULWHASOO', name: 'Sulwhasoo' },
+	{ value: 'SUQQU', name: 'SUQQU' },
+	{ value: 'TAKAMI', name: 'TAKAMI' },
+	{ value: 'THE HISTORY OF WHOO', name: 'The History of Whoo' },
+	{ value: 'THE ORCHID SKIN', name: 'The ORCHID Skin' },
+	{ value: 'TOM FORD', name: 'Tom Ford' },
+	{ value: 'TOO COOL FOR SCHOOL', name: 'Too Cool For School' },
+	{ value: 'TRILOGY', name: 'Trilogy' },
+	{ value: 'URBAN DECAY', name: 'Urban Decay' },
+	{ value: 'VIDIVICI', name: 'VIDIVICI' },
+	{ value: 'WAKEMAKE', name: 'Wakemake' },
+	{ value: 'YSL', name: 'YSL' }
+];
+
+export const JEWELRY_BRAND_DROPDOWN_LIST = [
+	{ value: 'VAN CLEEF & ARPELS', name: 'Van Cleef & Arpels' }
+];
+
+export const BAG_BRAND_DROPDOWN_LIST = [
+	{ value: 'HERMES', name: 'Hermes' },
+	{ value: 'BOTTEGA VENETA', name: 'Bottega Veneta' },
+	{ value: 'SAINT LAURENT', name: 'Saint Laurent' }
+];
